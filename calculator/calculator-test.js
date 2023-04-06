a625f157ebc9e5dfe871329302c905f41515a64c
Aseel Alnassiry <aseelalnassiry@gmail.com>
2023-04-06 16:47:11 -0700

    calculator testing complete

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,14 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+describe('calculate monthly rate correctly', () => {
+  it('should calculate the monthly rate correctly with interest', () => {
+    expect(calculateMonthlyPayment({ amount: 1000, years: 2, rate: 2 })).toEqual('42.54');
+  });
+  it('should calculate the monthly rate correctly with no interest', () => {
+    expect(calculateMonthlyPayment({ amount: 100000, years: 10, rate: 0 })).toEqual('833.33');
+  });
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it('should return a result with a number that has 2 decimal places', () => {
+  expect(calculateMonthlyPayment({ amount: 1000, years: 2, rate: 2 }).slice(-3, -2)).toBe('.');
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById("calc-form");
+window.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('calc-form');
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
       update();
     });
@@ -11,30 +11,52 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function getCurrentUIValues() {
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+    amount: +document.getElementById('loan-amount').value,
+    years: +document.getElementById('loan-years').value,
+    rate: +document.getElementById('loan-rate').value,
+  };
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const amount = document.getElementById('loan-amount');
+  const years = document.getElementById('loan-years');
+  const rate = document.getElementById('loan-rate');
+
+  amount.value = 1000;
+  years.value = 2;
+  rate.value = 2;
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const monthly = calculateMonthlyPayment(values);
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthly;
+  if (values.rate === 0) {
+    return (monthly = (values.amount / (values.years * 12)).toFixed(2));
+  }
+  let monthlyRate = values.rate / 100 / 12;
+  let numeator = values.amount * monthlyRate;
+  let denominator = 1 - Math.pow(1 + monthlyRate, -12 * values.years);
+  return (monthly = (numeator / denominator).toFixed(2));
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayment = document.getElementById('monthly-payment');
+  monthlyPayment.innerText = monthly;
 }
